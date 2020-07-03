5efb1c87544b9504bb2ade927c6c779c62e1edb6
Subrat Swain <Subrat.Swain@harman.com>
2020-07-03 15:21:43 +0530

    Logic implimented for Promotion

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PromotionEngine.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,32 @@ namespace PromotionEngine.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Unit unit)
+        {
+            unit.Result = Calculate(unit);
+            return View(unit);
+        }
+
+        public int Calculate(Unit unit)
+        {
+            int A = (unit.A / 3) * 130 + (unit.A % 3) * 50;
+            int B= (unit.B / 2) * 45 + (unit.B % 2) * 30;
+            int CD = 0;
+            if(unit.C==0 || unit.D == 0)
+            {
+                CD = (unit.C) * 20 + (unit.D) * 15;
+            }
+            else
+            {
+                int CDpair = Math.Min(unit.C, unit.D) * 30;
+                int remainder = Math.Max(unit.C, unit.D) - Math.Min(unit.C, unit.D);
+                CD=CDpair+ (Math.Max(unit.C, unit.D) == unit.C ? remainder * 20 : remainder * 15);
+            }
+            unit.Result = A + B + CD;
+            return unit.Result;
         }
     }
 }
