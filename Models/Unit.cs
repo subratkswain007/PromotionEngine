24be2ff2979031567766483d4207018ffaf7d2ee
Subrat Swain <Subrat.Swain@harman.com>
2020-07-03 14:09:31 +0530

    Model class for  promotion engine added

---
+++ Models/Unit.cs
@@ -0,0 +1,31 @@
+﻿namespace PromotionEngine.Models
+{
+    public class Unit
+    {
+        public int A
+        {
+            get;
+            set;
+        }
+        public int B
+        {
+            get;
+            set;
+        }
+        public int C
+        {
+            get;
+            set;
+        }
+        public int D
+        {
+            get;
+            set;
+        }
+        public int Result
+        {
+            get;
+            set;
+        }
+    }
+}
